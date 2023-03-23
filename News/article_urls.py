d998672e7c07f8584a1da92ec462b394ee7ece34
ezacd <119522254+ezacd@users.noreply.github.com>
2023-03-23 19:23:39 +0300

    Add files via upload

---
+++ News/article_urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import PostList, PostDetail, SearchList, ArticleCreate, ArticleUpdate, ArticleDelete, CategoryListView
+
+urlpatterns = [
+    path('', PostList.as_view()),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('search/', SearchList.as_view(), name='post_list'),
+    path('create/', ArticleCreate.as_view()),
+    path('<int:pk>/update/', ArticleUpdate.as_view(), name='article_update'),
+    path('<int:pk>/delete/', ArticleDelete.as_view(), name='article_delete'),
+    path('categories/<int:pk>', CategoryListView.as_view(), name='category_list')
+]
---
+++ News/views.py
@@ -0,0 +1,138 @@
+from datetime import datetime
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .filters import PostFilter
+from .models import Post, Category
+from .forms import PostForm
+
+from django.shortcuts import redirect, get_object_or_404, render
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+
+
+class PostList(LoginRequiredMixin, ListView):
+    model = Post
+    ordering = 'data'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['is_not_author'] = not self.request.user.groups.filter(name='authors').exists()
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        contex = super().get_context_data(**kwargs)
+        contex['time_now'] = datetime.utcnow()
+        return contex
+
+
+class SearchList(ListView):
+    model = Post
+    template_name = 'search_page.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        context['time_now'] = datetime.utcnow()
+        return context
+
+
+class NewsCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_create.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.post_type = 'NE'
+        return super().form_valid(form)
+
+
+class ArticleCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_create.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.post_type = 'AR'
+        return super().form_valid(form)
+
+
+class NewsUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_create.html'
+
+
+class ArticleUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_create.html'
+
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
+
+
+class ArticleDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
+
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    premium_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        premium_group.user_set.add(user)
+    return redirect('/news')
+
+
+class CategoryListView(ListView):
+    model = Post
+    template_name = 'category_list.html'
+    context_object_name = 'category_news_list'
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, id=self.kwargs['pk'])
+        queryset = Post.objects.filter(category=self.category).order_by('data')
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_subscriber'] = self.request.user not in self.category.subscribers.all()
+        context['category'] = self.category
+        return context
+
+
+@login_required
+def subscribe(request, pk):
+    user = request.user
+    category = Category.objects.get(id=pk)
+    category.subscribers.add(user)
+
+    massage = 'Вы подписались на рассылку новостей категории '
+    return render(request, 'subscribe.html', {'category': category, 'massage': massage})
